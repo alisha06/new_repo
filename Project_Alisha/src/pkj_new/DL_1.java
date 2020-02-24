ab6a7f287e22089aa7372ed3b862d4d84b01101a
Alisha.Chawla <Alisha.Chawla@IND00139.emea.akqa.local>
2020-02-24 18:13:07 +0530

    new

--- Project_Alisha/src/pkj_new/DL_1.java
+++ Project_Alisha/src/pkj_new/DL_1.java
@@ -1,5 +1,15 @@
 package pkj_new;
 
 public class DL_1 {
-
+	public static void main(String[] args) 
+	{
+		for (int i=5; i>=1; i--)
+		{
+			for (int j=1;j<=i; j++)
+			{
+			System.out.print("*");
+			}	
+			System.out.println();
+	}
+}
 }
---
+++ Project_Alisha/src/pkj_new/DL_2.java
@@ -0,0 +1,19 @@
+package pkj_new;
+
+public class DL_2 {
+	public static void main(String[] args) 
+	{
+	
+	int i,j,n=1;
+		for (i=1; i<=4; i++)
+		{
+			for (j=1;j<=i; j++)
+			{
+			System.out.print(+n);
+			n++;
+			}	
+			System.out.println();
+
+}
+	}
+	}
